cb98356b273e167e5c9f13a7509967f034c52d4e
Macintosh98 <abhishekzambare@gmail.com>
2020-11-22 19:50:07 +0530

    2.0

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
 // var mysql = require('mysql');
-
+const path =require('path')
 const app= express();
 
 app.use(bodyParser.json());
@@ -21,6 +21,16 @@ app.use(bodyParser.json());
 app.use('/users', require('./routes/users'));
 app.use('/registereduserdetails', require('./routes/registereduserdetails'));
 app.use('/SportDetails', require('./routes/SportDetails'));
+
+
+if(process.env.NODE_ENV == "production"){
+
+    app.use(express.static(client/build));
+
+    app.get("*",(req,res)=>{
+res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 
 const port= process.env.PORT || 5000;
