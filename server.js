cdbcb5a80f882e2f630dbb5159acdb9169a04bd4
Macintosh98 <abhishekzambare@gmail.com>
2020-11-22 21:01:39 +0530

    2.0

--- server.js
+++ server.js
@@ -7,30 +7,20 @@ const app= express();
 
 app.use(bodyParser.json());
 
-// var connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'redsportsz'
-// })
 
-// connection.connect(function(err) {
-//     if (err) throw err;
-//     else console.log("DB connected...");
-// });
-app.use('/users', require('./routes/users'));
-app.use('/registereduserdetails', require('./routes/registereduserdetails'));
-app.use('/SportDetails', require('./routes/SportDetails'));
+// app.use('/users', require('./routes/users'));
+// app.use('/registereduserdetails', require('./routes/registereduserdetails'));
+// app.use('/SportDetails', require('./routes/SportDetails'));
 
 
-if(process.env.NODE_ENV == "production"){
+// if(process.env.NODE_ENV == "production"){
 
-    app.use(express.static(client/build));
+    app.use(express.static("client/build"));
 
     app.get("*",(req,res)=>{
 res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
-}
+// }
 
 
 const port= process.env.PORT || 5000;
